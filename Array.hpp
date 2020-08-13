bd5bb28241b7904e160e28cea41f54f99ab30396
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-08-13 14:48:01 -0400

    Update Array.hpp

--- Array.hpp
+++ Array.hpp
@@ -1,24 +1,35 @@
+
+#pragma once
 #include <iostream>
-#ifndef Array_hpp
-#define Array_hpp
-template <class T>
-class Array{
-  
+#include <string>
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+
+template<typename T> class Array;
+template<typename T> std::istream& operator>>(std::istream& input, Array<T>& a);
+template <typename T> std::ostream& operator<<(std::ostream& output, const Array<T>& a);
+template<typename T>
+class Array
+{
+    friend std::istream& operator>> <>(std::istream& input, Array<T>& a);
+    friend std::ostream& operator<< <>(std::ostream& output, const Array<T>& a);
+ 
 public:
-    Array(int=10);
-    Array(const Array&);
+    Array(int arraySize);
+    Array(const Array& arrayTocopy);
     ~Array();
-    size_t getSize()const;
+    size_t getSize();
+    const Array& operator=(const Array& right);
+    bool operator==(const Array& right)const;
+    bool operator!=(const Array& right)const;
+    T& operator[](size_t subscript);
 
-    void add(const T&);
-    
-    T& get(size_t);
-    
+   
+
 private:
-    int elem;
     size_t size;
-    T *ptr;
+    T* ptr;
 };
-
-
-#endif /* Array_hpp */
+#endif
