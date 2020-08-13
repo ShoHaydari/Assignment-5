f93aa8d3c4fb5fdd1c78fe0cd22f389126c413c7
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-08-13 14:47:18 -0400

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,59 +1,80 @@
-
 #include "Array.hpp"
-#include <iostream>
 using namespace std;
-template <class T>
-Array<T>::Array(int arraySize){
-    if (arraySize>0) {
-        size=static_cast<size_t>(arraySize);
-        ptr=new T[size];
-        for (size_t i=0; i<size; i++) {
-        }
 
-    }
-    else
-    throw invalid_argument("Array size must be bigger that 0");
-    
-}
+template<typename T>
+Array<T>::Array(int arraySize) :size{ (arraySize > 0 ? static_cast<size_t>(arraySize) : throw invalid_argument{"Array must be greater than 0"}) }, ptr{ new T[size]{} }
+{/* empty body */}
 
-template <class T>
-Array<T>::Array(const Array& a){ 
-    
-    size=a.size;
-    ptr=new T[size];
-    
-    for (size_t i=0; i<0; i++) {
-        ptr[i]=a.ptr[i];
+template<typename T>
+Array<T>::Array(const Array& arrayTocopy) {
+    size = arrayTocopy.size;
+    ptr = new T[size];
+
+    for (size_t i = 0; i < size; i++) {
+        ptr[i] = arrayTocopy.ptr[i];
     }
 }
 
-template <class T>
-Array<T>::~Array(){
-    delete [] ptr;
+template<typename T>
+Array<T>::~Array() {
+    size = 0;
+    delete[] ptr;
 }
 
-template <class T>
-void Array<T>::add(const T &obj){
-    if (elem<size) {
-        ptr[elem++]=obj;
-    }
-    
-    else
-        cout<<"Array is full"<<endl;
-}
-
-template <class T>
-T& Array<T>::get(size_t i){
-    if (i<0 || i>=size) {
-        throw "Out of range";
-    }
-    
-    else
-        return ptr[i];
-}
-
-template <class T>
-size_t Array<T>::getSize()const{
+template<typename T>
+size_t Array<T>::getSize() {
     return size;
+}
+
+template<typename T>
+const Array<T>& Array<T>::operator=(const Array& right) {
+    delete[] ptr;
+    size = right.size;
+    ptr = new T[right.size];
+
+    for (size_t i = 0; i < size; i++) {
+        ptr[i] = right.ptr[i];
+    }
+    return *this;
+}
+
+template<typename T>
+bool Array<T>::operator==(const Array& right)const {
+    if (size != right.size)
+        return false;
+
+    for (size_t i = 0; i < size; i++) {
+        if (ptr[i] != right.ptr[i])
+            return false;
+    }
+    return true;
+}
+
+template<typename T>
+bool Array<T>::operator!=(const Array& right)const {
+    return !(*this == right);
+}
+
+template<typename T>
+T& Array<T>::operator[](size_t subscript) {
+    if (subscript < 0 || subscript >= size)
+        throw("subscript out of range\n");
+    return ptr[subscript];
+}
+
+template<typename T>
+istream& operator>>(istream& input, Array<T>& a) {
+    for (size_t i = 0; i < a.size; i++) {
+        input >> a.ptr[i];
+    }
+    return input;
+}
+
+template<typename T>
+ostream& operator<< (ostream& output, const Array<T>& a) {
+    for (size_t i = 0; i < a.size; i++) {
+        output << a.ptr[i] << "\n";
+    }
+    return output;
 }
 
