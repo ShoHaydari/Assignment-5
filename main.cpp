4a5502d0cdda26e0fbdbc2a6f3be3b69a43e1ce4
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-08-13 14:46:48 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,79 +1,102 @@
-#include "Array.hpp"
-#include "Array.cpp"
 #include <iostream>
+#include "Array.hpp"
 #include <fstream>
+#include "Array.cpp"
 #include <string>
 
-int main() {
-    
-    Array<int>intArray{5}; 
-    Array<double>doubleArray{5};
-    Array<string>stringArray{3};
-    
-    ofstream theFile("Template.txt", ios::out);
+using namespace std;
 
-cout<<"Please enter 5 integers to initialise integer array:"<<endl;
-    theFile<<"Integer Array: "<<endl;
-    for (size_t i=0; i<intArray.getSize(); i++) {
-        int a;
-        cin>>a;
-        intArray.add(a);
-        theFile<<a<<" ";
-        
+int main(){
+
+
+    Array<int>intArray{ 5 };
+    Array<double>doubleArray{ 5 };
+    Array<string>stringArray{ 3 };
+
+    ofstream myfile("Templates.txt", ios::out); //Templates.txt will store the values inputed in a .txt file
+
+    //Testing  [], ==, <<, and >>
+    cout << "Testing [], ==, <<, and >> " << endl;
+    cout << "Enter elements in Int array\n";
+    myfile << "Testing [], ==, <<, and >> " << endl;
+   myfile << "Int Array Inputs" << endl;
+    for (size_t i = 0; i < 5; i++) {
+        cin >> intArray[i];
+        myfile << intArray[i]<<" " ;
+        if (i + 1 == intArray.getSize()){
+            cout << "\nYour Int Array" << endl;
+            cout << intArray ;
+        }
+            
     }
-    theFile<<endl;
-    theFile<<endl;
+    myfile<<endl;
+
     
-    cout<<"Array after initialization: "<<endl;
-    for (size_t i=0; i<intArray.getSize(); i++) {
-        cout<<intArray.get(i)<<" ";
+    cout << "\nEnter elements in Double Array" << endl;
+    myfile << "Double Array Inputs" << endl;
+    for (size_t i = 0; i < 5; i++) {
+        cin >> doubleArray[i];
+        myfile << doubleArray[i]<<" ";
+        if (i + 1 == doubleArray.getSize()){
+            cout << "\nYour Double Array" << endl;
+            cout << doubleArray ;
+        }
+            
+    }
+    myfile<<endl;
     
+    cout << "\n\nEnter elements in string array\n";
+   myfile << "String Array Inputs" << endl;
+    for (size_t i = 0; i < 3; i++) {
+        cin >> stringArray[i];
+        myfile << stringArray[i] <<" ";
+        if (i + 1 == stringArray.getSize()){
+            cout << "\nYour String Array" << endl;
+            cout << stringArray ;
+        }
+            
+    }
+
+
+    myfile<<endl;
+    myfile<<endl;
+    
+
+    Array<double>copiedarray{ doubleArray };
+    cout << "Testing copy constructor and !=" << endl;
+    myfile<< "Testing copy constructor and !=" << endl;
+    for (size_t i = 0; i < copiedarray.getSize(); i++) {
+        cout << copiedarray[i] << endl;
+        myfile << copiedarray[i] << " ";
+    }
+
+    if (copiedarray == doubleArray) {
+        cout << "\nthe Double Array was succesfully copied" << endl;
+        myfile << "\nthe Double Array was succesfully copied" << endl;
+    }
+
+    for(size_t i = 0; i < copiedarray.getSize(); i++){
+        if (copiedarray[i] != intArray[i]) {
+            cout << "the copied array element in position " << i << " is not the same as in the int array" << endl;
+        myfile << "the copied array element in position " << i << " is not the same as in the int array" << endl;
+        }
+        else{
+            cout << "the copied array element in position " << i << " is the same as the int array" << endl;
+            myfile << "the copied array element in position " << i << " is the same as in the int array" << endl;
+        }
     }
     cout<<endl;
+    
+    cout<<"Reading from txt file:"<<endl;
     cout<<endl;
-    
-    cout<<"Double array size: "<<doubleArray.getSize()<<endl;
-        
-     
-        cout<<endl;
-
-
-    cout<<"Please enter 5 doubles to initialise double array:"<<endl;
-    theFile<<"Double Array: "<<endl;
-        for (size_t i=0; i<intArray.getSize(); i++) {
-            double a;
-            cin>>a;
-            doubleArray.add(a);
-            theFile<<a<<" ";
+    string line;
+    ifstream theFile("Templates.txt", ios::in);
+  
+    if (theFile.is_open()) {
+        while (getline(theFile,line)) {
+            cout<<line<<endl;
         }
-    theFile<<endl;
-    theFile<<endl;
-        cout<<"Array after initialization: "<<endl;
-        for (size_t i=0; i<intArray.getSize(); i++) {
-            cout<<doubleArray.get(i)<<" ";
-
-        }
-        cout<<endl;
-        cout<<endl;
-    
-    cout<<"String array size: "<<stringArray.getSize()<<endl;
-    
-
-    cout<<"Please enter 3 strings to initialise string array:"<<endl;
-    theFile<<"String Array: "<<endl;
-        for (size_t i=0; i<stringArray.getSize(); i++) {
-            string a;
-            cin>>a;
-            stringArray.add(a);
-            theFile<<a<<" ";
-        }
-    theFile<<endl;
-    theFile<<endl;
-        
-    cout<<"Array after initialization: "<<endl;
-        for (size_t i=0; i<stringArray.getSize(); i++) {
-            cout<<stringArray.get(i)<<" ";
-        }
-        cout<<endl;
-        cout<<endl;
+        theFile.close();
+    }
+    return 0;
 }
